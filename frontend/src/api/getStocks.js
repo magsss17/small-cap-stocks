5ea8add6b7ea0558d35cc8d2fd9267dd0e2dbf46
Maggie <mschwier@wharton.upenn.edu>
2023-07-28 15:17:18 +0000

    Update links

--- frontend/src/api/getStocks.js
+++ frontend/src/api/getStocks.js
@@ -1,29 +1,29 @@
 const getStocksSymbol = async () => {
-  const response = await fetch('/stock-filter-symbol');
+  const response = await fetch('http://ec2-54-243-141-88.compute-1.amazonaws.com:8080/stock-filter-symbol');
   const data = await response.json();
   return data;
 };
 
 const getStocksName = async () => {
-  const response = await fetch('/stock-filter-name');
+  const response = await fetch('http://ec2-54-243-141-88.compute-1.amazonaws.com:8080/stock-filter-name');
   const data = await response.json();
   return data;
 };
 
 const getStocksGrowth = async () => {
-  const response = await fetch('/stock-filter-growth');
+  const response = await fetch('http://ec2-54-243-141-88.compute-1.amazonaws.com:8080/stock-filter-growth');
   const data = await response.json();
   return data;
 };
 
 const getStocksSector = async () => {
-  const response = await fetch('/stock-filter-sector');
+  const response = await fetch('http://ec2-54-243-141-88.compute-1.amazonaws.com:8080/stock-filter-sector');
   const data = await response.json();
   return data;
 };
 
 const getStocksPrice = async () => {
-  const response = await fetch('/stock-filter-price');
+  const response = await fetch('http://ec2-54-243-141-88.compute-1.amazonaws.com:8080/stock-filter-price');
   const data = await response.json();
   return data;
 };
