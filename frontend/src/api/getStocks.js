2defbe33cec32f7f5449b0177348e45ecea98ff8
Johnny Ramirez <ramirezjohnny@icloud.com>
2023-08-04 14:59:59 +0000

    Update frontend with new api endpoints

--- frontend/src/api/getStocks.js
+++ frontend/src/api/getStocks.js
@@ -1,52 +1,16 @@
-const getStocksSymbol = async () => {
-  const response = await fetch('/stock-filter-symbol');
+const fetchStocks = async () => {
+  const response = await fetch('/stocks');
   const data = await response.json();
   return data;
 };
 
-const getStocks = async () => {
-  const response = await fetch('/stock-filter-name');
-  const data = await response.json();
-  return data;
-};
-
-const getStocksGrowth = async () => {
-  const response = await fetch('/stock-filter-growth');
-  const data = await response.json();
-  return data;
-};
-
-const getStocksSector = async () => {
-  const response = await fetch('/stock-filter-sector');
-  const data = await response.json();
-  return data;
-};
-
-const getStocksPrice = async () => {
-  const response = await fetch('/stock-filter-price');
-  const data = await response.json();
-  return data;
-};
-
-const getStocksIndustry = async () => {
-  const response = await fetch('/stock-filter-industry');
-  const data = await response.json();
-  return data;
-};
-
-const fetchStockDetails = async (symbol) => {
-  const response = await fetch(`/stock-details?symbol=${symbol}`);
-  const data = await response.json();
-  return data;
-};
-
-const fetchStockFinancials = async (symbol) => {
-  const response = await fetch(`/stock-financials?symbol=${symbol}`);
+const fetchStock = async (symbol) => {
+  console.log(`Fetching stock ${symbol}`);
+  const response = await fetch(`/stock?symbol=${symbol}`);
   const data = await response.json();
   return data;
 };
 
 export {
-  getStocksSymbol, getStocks, getStocksGrowth, getStocksSector, getStocksPrice,
-  getStocksIndustry, fetchStockDetails, fetchStockFinancials,
+  fetchStock, fetchStocks,
 };
